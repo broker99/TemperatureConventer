0948b8c6934ba1e823be054c7feb1ef4f291ea59
broker99 <60880830+broker99@users.noreply.github.com>
2020-05-15 06:55:31 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -56,14 +56,22 @@ function reset(){  // очищающая ф-ия
       
 };
 
-function digitalWatch() {
-var date = new Date();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
-if (hours < 10) hours = "0" + hours;
-if (minutes < 10) minutes = "0" + minutes;
-if (seconds < 10) seconds = "0" + seconds;
-document.getElementById("digital_watch").innerHTML = hours + ":" + minutes + ":" + seconds;
-setTimeout("digitalWatch()", 1000);
+function digitalWatch() { // часы
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    
+    document.getElementById("digital_watch").innerHTML = hours + ":" + minutes + ":" + seconds;
+    setTimeout("digitalWatch()", 1000);
 }
